ab7b75c7978561fd8227540b300ab5458902e7ed
unmatched78 <marvinavi24@gmail.com>
2025-06-18 00:51:33 +0200

    debug

--- config/settings.py
+++ config/settings.py
@@ -193,7 +193,7 @@ from corsheaders.defaults import default_headers  # if you need custom headers
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-    "cyclesync-beta.vercel.app",
+    #"cyclesync-beta.vercel.app",
     "back-cyclesync.vercel.app"
     # you can add more defaults or hosts here
    #"*"
